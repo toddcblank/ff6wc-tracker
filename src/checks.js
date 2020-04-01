4cba0202d184436a94a21a959abfb1996c9330cb
Todd Blank <tblank@tx-tblank-ml.attlocal.net>
2020-04-01 15:56:12 -0500

    fixing description for figaro castle checks

--- src/checks.js
+++ src/checks.js
@@ -15,22 +15,26 @@ let moblizWor = {
     itemIcon: "./icons/phunbaba.gif",
     itemName: "Phunbaba",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 let sealedGate = {
     itemIcon: "./icons/maduin.gif",
     itemName: "Sealed Gate",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let whelk = {
     itemIcon: "./icons/whelk.gif",
     itemName: "Whelk",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let zozo = {
     itemIcon: "./icons/ramuh.gif",
     itemName: "Zozo",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 
 let strago = {
     itemIcon: "./icons/strago.gif",
@@ -43,17 +47,20 @@ let burningHouse = {
     itemIcon: "./icons/flame-eater.gif",
     itemName: "Burning House",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let ebotsRock = {
     itemIcon: "./icons/ebots-rock.gif",
     itemName: "Ebot's Rock",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 let fanaticsTower = {
     itemIcon: "./icons/fanatics-tower.gif",
     itemName: "Fanatics Tower",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 
 
 
@@ -68,7 +75,7 @@ let loneWolf = {
     itemIcon: "./icons/lone-wolf.gif",
     itemName: "Lone Wolf",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]   
+    itemAvailability: ["wob"]
 }
 
 let relm = {
@@ -82,12 +89,14 @@ let esperMountain = {
     itemIcon: "./icons/goddess-statue.gif",
     itemName: "Esper Mountain",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let owzersMansion = {
     itemIcon: "./icons/owzer.gif",
     itemName: "Owzer's Mansion",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 
 
 let gogo = {
@@ -98,11 +107,11 @@ let gogo = {
 }
 
 let zoneEater = {
-    
+
     itemIcon: "./icons/zone-eater.gif",
     itemName: "Zone Eater",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]  
+    itemAvailability: ["wor"]
 }
 
 let setzer = {
@@ -116,12 +125,14 @@ let darylsTomb = {
     itemIcon: "./icons/darillstomb.gif",
     itemName: "Darill's Tomb",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 let kohlingen = {
     itemIcon: "./icons/interceptor.gif",
     itemName: "Kohlingen",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["both"]}
+    itemAvailability: ["both"]
+}
 
 let edgar = {
     itemIcon: "./icons/edgar.gif",
@@ -134,17 +145,20 @@ let ancientCastle = {
     itemIcon: "./icons/odin.gif",
     itemName: "Ancient Castle",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 let figaroCastleWob = {
     itemIcon: "./icons/doma-castle-wob.png",
-    itemName: "Doma Castle WoB",
+    itemName: "Figaro Castle WoB",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["both"]}
+    itemAvailability: ["both"]
+}
 let figaroCastleWor = {
     itemIcon: "./icons/tentacle.gif",
-    itemName: "Doma Castle WoR",
+    itemName: "Figaro Castle WoR",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 
 let gau = {
     itemIcon: "./icons/gau.gif",
@@ -157,12 +171,14 @@ let serpentTrench = {
     itemIcon: "./icons/serpent-trench.gif",
     itemName: "Serpent Trench",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let veldt = {
     itemIcon: "./icons/gau-veldt.gif",
     itemName: "Gau on Veldt",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["both"]}
+    itemAvailability: ["both"]
+}
 
 let cyan = {
     itemIcon: "./icons/cyan.gif",
@@ -205,7 +221,8 @@ let narsheWeaponShop = {
     itemIcon: "./icons/ragnarok.gif",
     itemName: "Narshe Weapon Shop WoR",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
+    itemAvailability: ["wor"]
+}
 let phoenixCave = {
     itemIcon: "./icons/phoenix.gif",
     itemName: "Phoenix Cave",
@@ -265,26 +282,26 @@ let shadow = {
 
 }
 
-let fcAirBase = {    
+let fcAirBase = {
     itemIcon: "./icons/fc-airbase.gif",
     itemName: "Floating Continent - Air Base",
     itemClickStates: ["uncollected", "collected"],
     itemAvailability: ["wob"]
 }
 
-let fcAtma = {    
+let fcAtma = {
     itemIcon: "./icons/atma.gif",
     itemName: "Floating Continent Atma",
     itemClickStates: ["uncollected", "collected"],
     itemAvailability: ["wob"]
 }
-let fcWait = {    
+let fcWait = {
     itemIcon: "./icons/shadow.gif",
     itemName: "Wait for Shadow!",
     itemClickStates: ["uncollected", "collected"],
     itemAvailability: ["wob"]
 }
-let gausFather = {    
+let gausFather = {
     itemIcon: "./icons/gaus-dad.gif",
     itemName: "Gaus Father",
     itemClickStates: ["uncollected", "collected"],
@@ -294,8 +311,9 @@ let veldtCaveWor = {
     itemIcon: "./icons/srbehemoth.gif",
     itemName: "Veldt WoR",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
-    
+    itemAvailability: ["wor"]
+}
+
 let sabin = {
     itemIcon: "./icons/sabin.gif",
     itemName: "Sabin",
@@ -303,31 +321,36 @@ let sabin = {
     itemAvailability: ["character"]
 }
 
-let barenFalls      = {
+let barenFalls = {
     itemIcon: "./icons/baren-falls.gif",
     itemName: "Baren Falls",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 let collapsingHouse = {
     itemIcon: "./icons/burningHouse.gif",
     itemName: "Collapsing House",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]}
-let imperialCamp    = {
+    itemAvailability: ["wor"]
+}
+let imperialCamp = {
     itemIcon: "./icons/imperial-camp.gif",
     itemName: "Imperial Camp",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
-let mtKolts         = {
+    itemAvailability: ["wob"]
+}
+let mtKolts = {
     itemIcon: "./icons/vargas.gif",
     itemName: "Vargas",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
-let phantomTrain        = {
+    itemAvailability: ["wob"]
+}
+let phantomTrain = {
     itemIcon: "./icons/ghost-train.gif",
     itemName: "Ghost Train",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wob"]}
+    itemAvailability: ["wob"]
+}
 
 let umaro = {
     itemIcon: "./icons/umaro.gif",
@@ -340,7 +363,7 @@ let umaroBoss = {
     itemIcon: "./icons/umaro-boss.gif",
     itemName: "Umaro (boss)",
     itemClickStates: ["uncollected", "collected"],
-    itemAvailability: ["wor"]   
+    itemAvailability: ["wor"]
 }
 
 let open = {
@@ -348,42 +371,42 @@ let open = {
     itemName: "Free",
     itemClickStates: ["collected"],
 }
-    let tzen = {
-        itemIcon: "./icons/tzen-woods.gif",
-        itemName: "Woods Dealer",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["both"]
-    }
-    let tritoch = {
-        itemIcon: "./icons/tritoch.gif",
-        itemName: "Tritoch",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
-    let kefkaAtNarshe = {
-        itemIcon: "./icons/kefka-narshe.gif",
-        itemName: "Kefka At Narshe",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wob"]
-    }
-    let doomGaze = {
-        itemIcon: "./icons/doomgaze.gif",
-        itemName: "DoomGaze",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
-    let auctionHouseOne = {
-        itemIcon: "./icons/auction-house.gif",
-        itemName: "Auction House 1",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["both"]
-    }
-    let auctionHouseTwo = {
-        itemIcon: "./icons/auction-house.gif",
-        itemName: "Auction House 2",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["both"]
-    }
+let tzen = {
+    itemIcon: "./icons/tzen-woods.gif",
+    itemName: "Woods Dealer",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["both"]
+}
+let tritoch = {
+    itemIcon: "./icons/tritoch.gif",
+    itemName: "Tritoch",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
+let kefkaAtNarshe = {
+    itemIcon: "./icons/kefka-narshe.gif",
+    itemName: "Kefka At Narshe",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wob"]
+}
+let doomGaze = {
+    itemIcon: "./icons/doomgaze.gif",
+    itemName: "DoomGaze",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
+let auctionHouseOne = {
+    itemIcon: "./icons/auction-house.gif",
+    itemName: "Auction House 1",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["both"]
+}
+let auctionHouseTwo = {
+    itemIcon: "./icons/auction-house.gif",
+    itemName: "Auction House 2",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["both"]
+}
 
 let dragonsGroup = {
     itemIcon: "./icons/dragon.gif",
@@ -391,150 +414,150 @@ let dragonsGroup = {
     itemClickStates: ["collected"],
 }
 
-    let blueDragon = {
-        itemIcon: "./icons/blue-dragon.gif",
-        itemName: "Blue Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let blueDragon = {
+    itemIcon: "./icons/blue-dragon.gif",
+    itemName: "Blue Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let dirtDragon = {
-        itemIcon: "./icons/dirt-dragon.gif",
-        itemName: "Dirt Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let dirtDragon = {
+    itemIcon: "./icons/dirt-dragon.gif",
+    itemName: "Dirt Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let goldDragon = {
-        itemIcon: "./icons/gold-dragon.gif",
-        itemName: "Gold Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let goldDragon = {
+    itemIcon: "./icons/gold-dragon.gif",
+    itemName: "Gold Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let iceDragon = {
-        itemIcon: "./icons/ice-dragon.gif",
-        itemName: "Ice Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let iceDragon = {
+    itemIcon: "./icons/ice-dragon.gif",
+    itemName: "Ice Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let redDragon = {
-        itemIcon: "./icons/red-dragon.gif",
-        itemName: "Red Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let redDragon = {
+    itemIcon: "./icons/red-dragon.gif",
+    itemName: "Red Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let skullDragon = {
-        itemIcon: "./icons/skull-dragon.gif",
-        itemName: "Skull Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let skullDragon = {
+    itemIcon: "./icons/skull-dragon.gif",
+    itemName: "Skull Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let stormDragon = {
-        itemIcon: "./icons/storm-dragon.gif",
-        itemName: "Storm Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let stormDragon = {
+    itemIcon: "./icons/storm-dragon.gif",
+    itemName: "Storm Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
-    let whiteDragon = {
-        itemIcon: "./icons/white-dragon.gif",
-        itemName: "White Dragon",
-        itemClickStates: ["uncollected", "collected"],
-        itemAvailability: ["wor"]
-    }
+let whiteDragon = {
+    itemIcon: "./icons/white-dragon.gif",
+    itemName: "White Dragon",
+    itemClickStates: ["uncollected", "collected"],
+    itemAvailability: ["wor"]
+}
 
 
-    let trackerJson = [
-        {
-          groupIcon: strago,
-          groupedItems: [burningHouse, ebotsRock, fanaticsTower]
-        },{
-          groupIcon: mog,
-          groupedItems: [loneWolf]
-        },
-        {
-            groupIcon: umaro,
-            groupedItems: [umaroBoss]
-        },{
-          groupIcon: relm,
-          groupedItems: [esperMountain, owzersMansion]
-        },{
-          groupIcon: gogo,
-          groupedItems: [zoneEater]
-        },{
-          groupIcon: setzer,
-          groupedItems: [kohlingen, darylsTomb]
-        },{
-          groupIcon: edgar,
-          groupedItems: [figaroCastleWob, ancientCastle, figaroCastleWor]
-        },{
-          groupIcon: gau,
-          groupedItems: [serpentTrench, veldt]
-        },{
-          groupIcon: cyan,
-          groupedItems: [domaWob, domaWor, mtZozo]
-        },{
-          groupIcon: locke,
-          groupedItems: [
-            southFigaroCave, narsheWeaponShop, phoenixCave, ]
-        },{
-          groupIcon: celes,
-          groupedItems: [
-            magitekIfrit, 
-            magitek24, 
-            magitekCrane, 
-            operaHouse, 
+let trackerJson = [
+    {
+        groupIcon: strago,
+        groupedItems: [burningHouse, ebotsRock, fanaticsTower]
+    }, {
+        groupIcon: mog,
+        groupedItems: [loneWolf]
+    },
+    {
+        groupIcon: umaro,
+        groupedItems: [umaroBoss]
+    }, {
+        groupIcon: relm,
+        groupedItems: [esperMountain, owzersMansion]
+    }, {
+        groupIcon: gogo,
+        groupedItems: [zoneEater]
+    }, {
+        groupIcon: setzer,
+        groupedItems: [kohlingen, darylsTomb]
+    }, {
+        groupIcon: edgar,
+        groupedItems: [figaroCastleWob, ancientCastle, figaroCastleWor]
+    }, {
+        groupIcon: gau,
+        groupedItems: [serpentTrench, veldt]
+    }, {
+        groupIcon: cyan,
+        groupedItems: [domaWob, domaWor, mtZozo]
+    }, {
+        groupIcon: locke,
+        groupedItems: [
+            southFigaroCave, narsheWeaponShop, phoenixCave,]
+    }, {
+        groupIcon: celes,
+        groupedItems: [
+            magitekIfrit,
+            magitek24,
+            magitekCrane,
+            operaHouse,
             southFigaro]
-        },{
-            groupIcon: shadow,
-            groupedItems: [
-                gausFather,
-                fcAirBase, fcAtma, fcWait, 
-                veldtCaveWor
-            ]
-          },{
-          groupIcon: sabin,
-          groupedItems: [
+    }, {
+        groupIcon: shadow,
+        groupedItems: [
+            gausFather,
+            fcAirBase, fcAtma, fcWait,
+            veldtCaveWor
+        ]
+    }, {
+        groupIcon: sabin,
+        groupedItems: [
             barenFalls,
-            imperialCamp,    
-            mtKolts,         
+            imperialCamp,
+            mtKolts,
             phantomTrain,
             collapsingHouse,
-            ]
-        },{
-            groupIcon: terra,
-            groupedItems: [
-                leteRiver, 
-                sealedGate, 
-                whelk, 
-                zozo, 
-                moblizWor]
-          },{
-          groupIcon: open,
-          groupedItems: [
-            kefkaAtNarshe, 
-              tzen, 
-              auctionHouseOne, 
-              auctionHouseTwo,
-              tritoch,
-              doomGaze, ]
-        },{
-            groupIcon: dragonsGroup,
-            groupedItems: [
-                blueDragon,
-                dirtDragon,
-                goldDragon,
-                iceDragon,
-                redDragon,
-                skullDragon,
-                stormDragon,
-                whiteDragon,]
-          },
-      ]
+        ]
+    }, {
+        groupIcon: terra,
+        groupedItems: [
+            leteRiver,
+            sealedGate,
+            whelk,
+            zozo,
+            moblizWor]
+    }, {
+        groupIcon: open,
+        groupedItems: [
+            kefkaAtNarshe,
+            tzen,
+            auctionHouseOne,
+            auctionHouseTwo,
+            tritoch,
+            doomGaze,]
+    }, {
+        groupIcon: dragonsGroup,
+        groupedItems: [
+            blueDragon,
+            dirtDragon,
+            goldDragon,
+            iceDragon,
+            redDragon,
+            skullDragon,
+            stormDragon,
+            whiteDragon,]
+    },
+]
 
 
 export default trackerJson
